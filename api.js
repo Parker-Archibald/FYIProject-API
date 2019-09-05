b7f36f3b9ce631a923ba80d7c215176a40ce06ed
parker <parkerarchibald97@gmail.com>
2019-09-05 11:21:01 -0600

    fyi api initial commit

---
+++ api.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb+srv://pdarchibald:mlarchibald@cluster0-vdsoz.mongodb.net/test?retryWrites=true&w=majority";
+const dbName = "FYI";
+const ObjectId = require("mongodb").ObjectId;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/leads", (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+    collection.find({status: 'potentialClient'}).toArray((err, docs) => {
+      client.close();
+      res.send(docs);
+    });
+  });
+});
+
+app.get("/clients", (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+    collection.find({status: 'currentClient'}).toArray((err, docs) => {
+      client.close();
+      res.send(docs);
+    });
+  });
+});
+
+app.post("/leads", (req, res) => {
+
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection("users");
+        collection.insertMany([
+          {
+            name: req.body.name,
+            email: req.body.email,
+            date: req.body.date,
+            phone: req.body.phone,
+            comments: req.body.comments,
+            status: req.body.status
+          }
+        ]);
+        client.close();
+        res.send("Added user");
+      });
+});
+
+app.put('/leads/:ID', (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName);
+    const collection = db.collection("users");
+    collection.updateOne({_id: ObjectId(req.params.ID)}, {$set: req.body});
+    client.close();
+    res.send("Updated");
+  });
+});
+
+app.delete("/leads/:ID", (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, async (err, client) => {
+    const db = client.db(dbName);
+    const collection = db.collection("users");
+    await collection.deleteOne({_id: ObjectId(req.params.ID)});
+    res.send("Deleted");
+
+    client.close();
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
